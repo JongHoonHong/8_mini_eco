aafe85592ce61765115382654da7f0aa0f4ad4ab
blueprint-12 <blueprint12@daum.net>
2022-06-13 16:53:08 +0900

    원희 세팅 완료

--- src/App.js
+++ src/App.js
@@ -3,17 +3,35 @@ import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import UploadPost from "./pages/UploadPost";
 import PostDetail from "./pages/PostDetail";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Header from "./components/Header";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/uploadpost" element={<UploadPost />} />
-        <Route path="/postdetail" element={<PostDetail />} />
-      </Routes>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/uploadpost" element={<UploadPost />} />
+          <Route path="/postdetail" element={<PostDetail />} />
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </Container>
     </div>
   );
 }
 
+const Container = styled.div`
+  /* display: flex; */
+  width: 1000px;
+  /* flex-direction: column;
+  justify-content: center;
+  align-items: center; */
+  font-size: calc(10px + 2vmin);
+  background-color: aliceblue;
+`;
 export default App;
